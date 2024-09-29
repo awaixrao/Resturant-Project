1b76ddca1688422d49d1cec4f5863b4c0223a7e7
Muhammad Awais <Awaixxxxx@gmail.com>
2024-09-29 19:01:17 +0500

    update about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -17,12 +17,18 @@ const About = () => {
                         <div className="col-md-6">
                             <div className='d-flex justify-content-center flex-column about-div'>
 
-                                <div>
-                                    <h2 className='about-heading text-white '>We Are Feane</h2>
-                                </div>
-                                <div>
-                                    <p className='text-white py-3'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All</p>
-                                </div>
+                            <div>
+    <h2 className='about-heading text-white'>We Are Aman Pizza Garden</h2>
+</div>
+<div>
+    <p className='text-white py-3'>
+        At Aman Pizza Garden, we take pride in serving delicious, high-quality pizzas made with fresh ingredients. 
+        Our menu features a wide variety of options, from classic Margherita to spicy pepperoni, and even unique gourmet creations that you won't find anywhere else. 
+        Whether you're dining in, taking out, or ordering delivery, we strive to provide an exceptional experience that keeps you coming back for more. 
+        Join us and taste the difference that passion and quality make!
+    </p>
+</div>
+
                                 <div className="">
 
                                     <button className="btn btn-warning py-2 px-5 rounded-pill text-white text-decoration-none">That's About Us...</button>
